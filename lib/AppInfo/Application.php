4a59644a2935b70b463ab70ff433dc9a0f6aba10
Boris Valero <val_b@orange.fr>
2026-06-17 16:02:08 +0200

    fix: register AppConfigRepository DI binding

--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -4,12 +4,15 @@ declare(strict_types=1);
 
 namespace OCA\FramaSpace\AppInfo;
 
+use OCA\FramaSpace\Domain\Port\AppConfigRepository;
+use OCA\FramaSpace\Infrastructure\Config\NextcloudAppConfigRepository;
 use OCA\FramaSpace\Listener\CSSInjectionListener;
 use OCP\AppFramework\App;
 use OCP\AppFramework\Bootstrap\IBootContext;
 use OCP\AppFramework\Bootstrap\IBootstrap;
 use OCP\AppFramework\Bootstrap\IRegistrationContext;
 use OCP\AppFramework\Http\Events\BeforeTemplateRenderedEvent;
+
 
 class Application extends App implements IBootstrap {
 	public const APP_ID = 'framaspace';
@@ -22,6 +25,10 @@ class Application extends App implements IBootstrap {
 
 	public function register(IRegistrationContext $context): void {
 		$context->registerEventListener(BeforeTemplateRenderedEvent::class, CSSInjectionListener::class);
+
+		$context->registerService(AppConfigRepository::class, function ($c) {
+			return $c->get(NextcloudAppConfigRepository::class);
+		});
 	}
 
 	public function boot(IBootContext $context): void {
